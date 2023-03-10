2810c32a1510fe5766a0357b0d23eba3f00098a7
GodSlayerXl <124649417+GodSlayerXl@users.noreply.github.com>
2023-03-09 20:21:43 -0600

    this is updated java code it is more effiecient thats has a hashmap

--- ScheduleGenerator.java
+++ ScheduleGenerator.java
@@ -1,124 +1,154 @@
-package scheduleGen;
+package scheduleGenertor;
 
-import java.io.File;
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Scanner;
+import java.util.*;
 
 class Activity {
-    private String name;
-    private String timeStart;
-    private String timeEnd;
-    private String day;
+    private final String name;
+    private final int startTime;
+    private final int endTime;
+    private final int dayOfWeek;
 
-    public Activity(String name, String timeStart, String timeEnd, String day) {
+    public Activity(String name, int startTime, int endTime, int dayOfWeek) {
         this.name = name;
-        this.timeStart = timeStart;
-        this.timeEnd = timeEnd;
-        this.day = day;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.dayOfWeek = dayOfWeek;
     }
 
     public String getName() {
         return name;
     }
 
-    public String getTimeStart() {
-        return timeStart;
+    public int getStartTime() {
+        return startTime;
     }
 
-    public String getTimeEnd() {
-        return timeEnd;
+    public int getEndTime() {
+        return endTime;
     }
 
-    public String getDay() {
-        return day;
+    public int getDayOfWeek() {
+        return dayOfWeek;
     }
 }
 
 class VerificationEngine {
     public static boolean checkOverlap(Activity activity1, Activity activity2) {
-        if (!activity1.getDay().equals(activity2.getDay())) {
+        if (activity1.getDayOfWeek() != activity2.getDayOfWeek()) {
             return false;
         }
 
-        String start1 = activity1.getTimeStart();
-        String end1 = activity1.getTimeEnd();
-        String start2 = activity2.getTimeStart();
-        String end2 = activity2.getTimeEnd();
+        int start1 = activity1.getStartTime();
+        int end1 = activity1.getEndTime();
+        int start2 = activity2.getStartTime();
+        int end2 = activity2.getEndTime();
 
-        if ((start1.compareTo(end2) <= 0) && (end1.compareTo(start2) >= 0)) {
-            return true;
-        } else {
-            return false;
-        }
+        return (start1 <= end2) && (end1 >= start2);
     }
 }
 
 public class ScheduleGenerator {
-    public static void main(String[] args) {
-        ArrayList<Activity> activities = new ArrayList<Activity>();
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
 
         // Get class times
-        System.out.println("Enter class times for Monday to Friday:");
+        Map<Integer, List<Activity>> activitiesByDay = new HashMap<>();
         for (int i = 1; i <= 5; i++) {
-            String day = getDay(1);
+            String day = getDay(i);
+            List<Activity> activities = new ArrayList<>();
             while (true) {
                 System.out.print("Class name for " + day + " (or enter \"done\" to move on): ");
                 String name = scanner.nextLine();
                 if (name.equals("done")) {
                     break;
                 }
-                System.out.print("Start time (in format HH:MM): ");
-                String startTime = scanner.nextLine();
-                System.out.print("End time (in format HH:MM): ");
-                String endTime = scanner.nextLine();
-                Activity activity = new Activity(name, startTime, endTime, day);
+                System.out.print("Start time (in format HHMM): ");
+                int startTime = scanner.nextInt();
+                System.out.print("End time (in format HHMM): ");
+                int endTime = scanner.nextInt();
+                scanner.nextLine();
+                Activity activity = new Activity(name, startTime, endTime, i);
                 activities.add(activity);
-                
-                String filename = "my_activities.txt";
-                writeActivitiesToFile(activities, filename);
             }
+            activitiesByDay.put(i, activities);
         }
 
-        // Get meal times --- only takes in one meal time and applies it to Each of the days
+        // Get meal times
         System.out.println("\nEnter meal times:");
-        System.out.print("Start time (in format HH:MM): ");
-        String mealStartTime = scanner.nextLine();
-        System.out.print("End time (in format HH:MM): ");
-        String mealEndTime = scanner.nextLine();
-        Activity mealActivity = new Activity("Meal", mealStartTime, mealEndTime, "Monday-Friday");
-        activities.add(mealActivity);
+        System.out.print("Start time (in format HHMM): ");
+        int mealStartTime = scanner.nextInt();
+        System.out.print("End time (in format HHMM): ");
+        int mealEndTime = scanner.nextInt();
+        scanner.nextLine();
+        Activity mealActivity = new Activity("Meal", mealStartTime, mealEndTime, 0);
+        for (int i = 1; i <= 5; i++) {
+            List<Activity> activities = activitiesByDay.get(i);
+            activities.add(mealActivity);
+        }
 
-        // Get bed times --- currently only takes in one time range. 
-        //Produces an error when given time range where start time is greater than end time.
+        // Get bed times
         System.out.println("\nEnter bed times:");
-        System.out.print("Start time (in format HH:MM): ");
-        String bedStartTime = scanner.nextLine();
-        System.out.print("End time (in format HH:MM): ");
-        String bedEndTime = scanner.nextLine();
-        Activity bedActivity = new Activity("Bed", bedStartTime, bedEndTime, "Monday-Friday");
-        activities.add(bedActivity);
+        System.out.print("Start time (in format HHMM): ");
+        int bedStartTime = scanner.nextInt();
+        System.out.print("End time (in format HHMM): ");
+        int bedEndTime = scanner.nextInt();
+        scanner.nextLine();
+        Activity bedActivity = new Activity("Bed", bedStartTime, bedEndTime, 0);
+        List<Activity> bedActivities = new ArrayList<>();
+        for (int i = 1; i <= 5; i++) {
+            bedActivities.add(bedActivity);
+        }
+        activitiesByDay.put(0, bedActivities);
 
         // Check for overlapping times
         boolean isOverlap = false;
-        for (int i = 0; i < activities.size(); i++) {
-            for (int j = i + 1; j < activities.size(); j++) {
-                if (VerificationEngine.checkOverlap(activities.get(i), activities.get(j))) {
-                    isOverlap = true;
-                    System.out.println("Error: Overlapping");
-                }}}}
-    //currently not working
-	private static void writeActivitiesToFile(ArrayList<Activity> activities, String filename) {
-		
-		
-	}
-	//functions
-	private static String getDay(int i) {
-		
-		return null;
-	}}
-    
+        Set<Activity> allActivities = new TreeSet<>(Comparator.comparing(Activity::
+        getStartTime).thenComparing(Activity::getDayOfWeek));
+        for (int i = 0; i <= 5; i++) {
+        List<Activity> activities = activitiesByDay.get(i);
+        allActivities.addAll(activities);
+        }
+        for (Activity activity1 : allActivities) {
+        for (Activity activity2 : allActivities) {
+        if (VerificationEngine.checkOverlap(activity1, activity2) && activity1 != activity2) {
+        isOverlap = true;
+        System.out.println("Error: Overlapping activities - " + activity1.getName() + " and " + activity2.getName());
+           }
+          }
+        }
+        
+           // Write activities to file
+    String filename = "my_activities.txt";
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+        for (Activity activity : allActivities) {
+            writer.write(activity.getName() + "," + activity.getStartTime() + "," + activity.getEndTime() + "," + activity.getDayOfWeek() + "\n");
+        }
+    } catch (IOException e) {
+        System.out.println("Error writing to file: " + e.getMessage());
+    }
+
+    if (!isOverlap) {
+        System.out.println("Schedule generated successfully!");
+    }
+}
+
+private static String getDay(int i) {
+    switch (i) {
+        case 1:
+            return "Monday";
+        case 2:
+            return "Tuesday";
+        case 3:
+            return "Wednesday";
+        case 4:
+            return "Thursday";
+        case 5:
+            return "Friday";
+        default:
+            return "";
+    }
+ }
+}
