24d5643487aee220552c7c41b1f6861047fe76f8
jooyu9882 <127456890+jooyu9882@users.noreply.github.com>
2023-04-06 14:15:24 -0500

    Update ScheduleGenerator.java

--- ScheduleGenerator.java
+++ ScheduleGenerator.java
@@ -66,7 +66,7 @@ public class ScheduleGenerator {
 // 		                {
 		                   
 // 		                }
-//                         else {method(inFIle.hasNext()}
+//                         else {method(inFIle.hasNext())}
 		               
 // 		            }
 
